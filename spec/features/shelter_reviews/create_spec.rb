aea873f6b1420393cde348815a090075c4e8f11e
Dan Sehl <dan.sehl@gmail.com>
2020-07-06 20:18:07 -0600

    Added to create_spec for shelter_reviews to test if an error message shows when not entering valid info

--- spec/features/shelter_reviews/create_spec.rb
+++ spec/features/shelter_reviews/create_spec.rb
@@ -30,4 +30,24 @@ RSpec.describe 'review create page' do
     expect(page).to have_content(content)
     expect(page).to have_content(rating)
   end
+
+  it "shows an error message if not filling out the create review fields properly" do
+    shelter_1 = Shelter.create!(name: "Joe's Shelter", address: "123 Apple St.", city: "Denver", state: "CO", zip: 80202)
+
+    title = "A great place"
+    rating = 4.5
+    content = "Had a great time"
+    image = "https://homepages.cae.wisc.edu/~ece533/images/cat.png"
+
+    visit "/shelters/#{shelter_1.id}/reviews/new"
+
+    fill_in :rating, with: rating
+    fill_in :content, with: content
+    fill_in :optional_image_link, with: image
+
+    click_button "Create Review"
+
+    expect(page).to have_content("This is an invalid review! Please re-enter.")
+    expect(page).to have_button('Create Review')
+  end
 end
