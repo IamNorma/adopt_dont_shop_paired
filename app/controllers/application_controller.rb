2b14eea044264c8787ac85a05d779a6004afa6d4
Dan Sehl <dan.sehl@gmail.com>
2020-07-07 20:44:11 -0600

    Add before_action to application_controller and private find_favorite_count method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :find_favorite_count
+
+  private
+
+  def find_favorite_count
+    @favorite_count = Favorite.all.count
+  end
 end
