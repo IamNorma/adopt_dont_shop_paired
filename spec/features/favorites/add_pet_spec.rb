7b2c10b670f93bd34f72475a1146c0e9214b2b14
Norma Lopez <norma_l304@yahoo.com>
2020-07-11 13:54:46 -0600

    Test user can't favorite a pet more than once

--- spec/features/favorites/add_pet_spec.rb
+++ spec/features/favorites/add_pet_spec.rb
@@ -52,6 +52,56 @@ RSpec.describe "When a user adds a pet to their favorites" do
       expect(page).to have_content("Pets Favorited: 2")
     end
   end
+
+  it "user can't favorite a pet more than once" do
+    shelter_1 = Shelter.create!(name: "Joe's Shelter", address: "123 Apple St.", city: "Denver", state: "CO", zip: 80202)
+    pet_1 = shelter_1.pets.create!(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/afghanhound_dog_pictures_.jpg", name: "Fido", approx_age: 3, sex: "F", shelter_name: shelter_1.name, description: "A furry friend!", status: true)
+    pet_2 = shelter_1.pets.create!(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/husky_sideways_dog_pictures_.jpg", name: "Zorba", approx_age: 2, sex: "M", shelter_name: shelter_1.name, status: true)
+
+    visit "/pets/#{pet_1.id}"
+
+    within(".clickables") do
+      expect(page).to have_button("Add Pet to Favorites")
+    end
+
+    visit '/pets'
+
+    within '.nav-bar' do
+      expect(page).to have_content("Pets Favorited: 0")
+    end
+
+    within("#pet-#{pet_1.id}") do
+      click_button "Add Pet to Favorites"
+    end
+
+    visit "/pets/#{pet_1.id}"
+
+    within '.nav-bar' do
+      expect(page).to have_content("Pets Favorited: 1")
+    end
+
+    within(".clickables") do
+      expect(page).to_not have_button("Add Pet to Favorites")
+      expect(page).to have_button("Remove From Favorites")
+      click_button "Remove From Favorites"
+    end
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+
+    within '.messages' do
+      expect(page).to have_content("You have removed this pet from your favorites")
+    end
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+
+    within(".clickables") do
+      expect(page).to have_button("Add Pet to Favorites")
+    end
+
+    within '.nav-bar' do
+      expect(page).to have_content("Pets Favorited: 0")
+    end
+
   it "displays link in nav bar to go to favorites index page" do
     shelter_1 = Shelter.create!(name: "Joe's Shelter", address: "123 Apple St.", city: "Denver", state: "CO", zip: 80202)
     pet_1 = shelter_1.pets.create!(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/afghanhound_dog_pictures_.jpg", name: "Fido", approx_age: 3, sex: "F", shelter_name: shelter_1.name, description: "A furry friend!", status: true)
