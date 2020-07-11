cd7e9e635a5cea8e92c8104691d1b9609fe0d477
Norma Lopez <norma_l304@yahoo.com>
2020-07-10 22:54:35 -0600

    Turn favorites indicator in nav bar into link

--- spec/features/favorites/add_pet_spec.rb
+++ spec/features/favorites/add_pet_spec.rb
@@ -52,4 +52,30 @@ RSpec.describe "When a user adds a pet to their favorites" do
       expect(page).to have_content("Pets Favorited: 2")
     end
   end
+  it "displays link in nav bar to go to favorites index page" do
+    shelter_1 = Shelter.create!(name: "Joe's Shelter", address: "123 Apple St.", city: "Denver", state: "CO", zip: 80202)
+    pet_1 = shelter_1.pets.create!(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/afghanhound_dog_pictures_.jpg", name: "Fido", approx_age: 3, sex: "F", shelter_name: shelter_1.name, description: "A furry friend!", status: true)
+    pet_2 = shelter_1.pets.create!(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/husky_sideways_dog_pictures_.jpg", name: "Zorba", approx_age: 2, sex: "M", shelter_name: shelter_1.name, status: true)
+
+    visit '/pets'
+
+    within("#pet-#{pet_1.id}") do
+      click_button "Add Pet to Favorites"
+    end
+
+    within("#pet-#{pet_2.id}") do
+      click_button "Add Pet to Favorites"
+    end
+
+    within '.nav-bar' do
+      expect(page).to have_content("Pets Favorited: 2")
+      expect(page).to have_link("Pets Favorited: 2")
+      click_on "Pets Favorited: 2"
+    end
+
+    expect(current_path).to eq('/favorites')
+
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_2.name)
+  end
 end
