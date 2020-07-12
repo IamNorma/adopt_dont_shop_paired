ff82c251e845e4735d45c0fb6160cf828854dfe5
Norma Lopez <norma_l304@yahoo.com>
2020-07-12 17:24:01 -0600

    And create method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,4 +6,21 @@ class ApplicationsController < ApplicationController
       end
     end.flatten
   end
+
+  def create
+    app = Application.new(application_params)
+    if app.save
+      flash[:notice] = "Application Submitted"
+      redirect_to "/favorites"
+      params[:pets].each do |pet_id|
+        favorites.contents.delete(pet_id.to_i)
+      end
+    end
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :phone, :description)
+  end
 end
