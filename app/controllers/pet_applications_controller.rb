480f25ddc5f3ea5384f23646279a1d8bc0e3ecd6
Norma Lopez <norma_l304@yahoo.com>
2020-07-13 20:49:49 -0600

    Add incomplete update action

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -2,4 +2,10 @@ class PetApplicationsController < ApplicationController
   def index
     @pet = Pet.find(params[:id])
   end
+
+  def update
+    pet = Pet.find(params[:pet_id])
+    app = Application.find(params[:application_id])
+    redirect_to "/pets/#{pet.id}"
+  end
 end
