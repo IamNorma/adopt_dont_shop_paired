7b4be92250d378d863103851a6a10ca2267ea770
Norma Lopez <norma_l304@yahoo.com>
2020-07-14 22:49:35 -0600

    Functionality to prevent deletion of a pet that has approved application

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -52,4 +52,38 @@ RSpec.describe "pets detail page", type: :feature do
     expect(page).to have_content("#{application2.name}")
     expect(page).to have_link("#{application2.name}")
   end
+
+  it 'does not allow deletion of a pet with approved application' do
+    shelter_1 = Shelter.create!(name: "Joe's Shelter", address: "123 Apple St.", city: "Denver", state: "CO", zip: 80202)
+    pet_1 = shelter_1.pets.create(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/afghanhound_dog_pictures_.jpg", name: "Fido", approx_age: 3, sex: "F", shelter_name: shelter_1.name, description: "A furry friend!", status: true)
+    pet_2 = shelter_1.pets.create!(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/husky_sideways_dog_pictures_.jpg", name: "Zorba", approx_age: 2, sex: "M", shelter_name: shelter_1.name, status: true)
+    application1 = Application.create!(name: "Bob",
+      address: "123 Fake St", city: "San Diego", state: "CA", zip: 92126,
+      phone: "123-456-7890", description: "I love animals!")
+    application2 = Application.create!(name: "Jill",
+      address: "1235 Fake St", city: "Denver", state: "CA", zip: 92126,
+      phone: "123-456-7890", description: "I love animals!")
+    PetApplication.create(pet: pet_1, application: application1)
+    PetApplication.create(pet: pet_2, application: application1)
+    PetApplication.create(pet: pet_2, application: application2)
+
+    visit "/applications/#{application1.id}"
+
+    within "#pet-#{pet_2.id}" do
+      expect(page).to have_link("Approve Application")
+      click_on "Approve Application"
+    end
+
+    visit "/pets/#{pet_2.id}"
+
+    within ".clickables" do
+      expect(page).to_not have_button("Delete Pet")
+    end
+
+    visit "/pets"
+
+    within ".pet-details" do
+      expect(page).to_not have_button("Delete #{pet_2.name}")
+    end
+  end
 end
